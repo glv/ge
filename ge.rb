ccc426d88aea6b9fd3a32573140e76fe9487adad
Glenn Vanderburg <glv@vanderburg.org>
2018-03-10 11:01:13 -0600

    generate stars and print star map
    ... and lose me a nerd bragging right I've held for a long time.
    
    (Although I didn't actually *write* this program, I'm just
    porting/translating it. Does that still count?)

--- ge.rb
+++ ge.rb
@@ -6,7 +6,7 @@
 # out of the original logic, those are placed at the bottom.
 module GE
   class Game
-    attr_reader :players, :worlds, :turns, :autobuild
+    attr_reader :players, :worlds, :turns, :autobuild, :stars
 
     def play
       print_rules
@@ -15,6 +15,7 @@ module GE
       puts "   worlds: #{worlds}"
       puts "    turns: #{turns}"
       puts "autobuild: #{autobuild}"
+      generate_star_locations
     end
 
     protected
@@ -43,23 +44,40 @@ module GE
         (1..100).include?(val)
       end
 
-      @autobuild = get_parameter("Do you want the neutral worlds to build defensive ships") do |input|
-        case input
-        when /^Y/ then true
-        when /^N/ then false
-        else throw(:invalid)
-        end
+      @autobuild = get_bool_parameter("Do you want the neutral worlds to build defensive ships")
+    end
+
+    # 2140
+    def generate_star_locations
+      loop do
+        x_coords = (1..20).to_a
+        y_coords = (1..20).to_a
+        @stars = x_coords.product(y_coords).sample(worlds)
+        print_star_map
+        repeat = get_bool_parameter("New setup")
+        break unless repeat
       end
     end
 
-    # These are all as-yet-unnamed procs that are the targets of GOSUBs
-    # 2140
-    def c
+    # 2240
+    # Rob Pike's bio (http://herpolhode.com/rob/) states that he "has
+    # never written a program that uses cursor addressing". While not
+    # as proud of it as he apparently is, I've always been pleased by
+    # the fact that I could say the same thing. Well, here goes
+    # nuthin'.
+    def print_star_map
+      # TODO: what does "CALL  - 936" do? (I'm guessing clear the screen)
+      clear_screen
+      cursor_position(1, 1)
+      print "*******STAR MAP*******"
+      stars.each_with_index do |(x, y), i|
+        cursor_position(x*2-1, y+1)
+        print i+1
+      end
+      cursor_position(1, 21)
     end
 
-    # 2240
-    def d
-    end
+    # These are all as-yet-unnamed procs that are the targets of GOSUBs
 
     # 2380
     def e
@@ -100,6 +118,10 @@ module GE
       print text.upcase + " "
     end
 
+    # Prompts the user for a game parameter. Yields the user's
+    # input to a block for validation and format conversion.
+    # The block must either return the validated parameter value
+    # or throw(:invalid).
     def get_parameter(prompt_string)
       loop do
         catch(:invalid) do
@@ -111,6 +133,10 @@ module GE
       end
     end
 
+    # Prompts the user for an integer game parameter. Yields
+    # the integer to a block for range validation; the block
+    # must return a truthy value if the parameter is valid,
+    # false otherwise.
     def get_int_parameter(prompt_string, &range_validation_proc)
       get_parameter(prompt_string) do |input|
         throw(:invalid) unless input =~ /^\d+$/
@@ -118,6 +144,24 @@ module GE
           throw(:invalid) unless range_validation_proc.(ival)
         }
       end
+    end
+
+    def get_bool_parameter(prompt_string)
+      get_parameter(prompt_string) do |input|
+        case input
+        when /^Y/ then true
+        when /^N/ then false
+        else throw(:invalid)
+        end
+      end
+    end
+
+    def clear_screen
+      print "\e[2J"
+    end
+
+    def cursor_position(x, y)
+      print "\e[#{y};#{x}H"
     end
   end
 end
