57a35cca84c5522a8c64e8413fda3f598a2435fc
Glenn Vanderburg <glv@vanderburg.org>
2018-03-12 09:20:56 -0500

    add most remaining game initialization steps
    Includes a revisit for uses of RND, based on finally finding documentation on that function indicating that the result will *not* include 1.0: <http://www.calormen.com/jsbasic/reference.html#Numeric-Functions>
    
    (Although that's not the official documentation, so I'm still not
    sure. That's how I *expected* the function to work, though, which is
    encouraging.)

--- ge.rb
+++ ge.rb
@@ -6,7 +6,10 @@
 # out of the original logic, those are placed at the bottom.
 module GE
   class Game
-    attr_reader :players, :worlds, :turns, :autobuild, :stars
+    attr_reader :players, :worlds, :turns       # integers
+    attr_reader :autobuild                      # booleans
+    attr_reader :player_worlds, :extra_worlds   # ranges
+    attr_reader :stars, :industries, :controls  # arrays
 
     def play
       print_rules
@@ -16,6 +19,9 @@ module GE
       puts "    turns: #{turns}"
       puts "autobuild: #{autobuild}"
       generate_star_locations
+      generate_industries
+      set_initial_world_control
+      build_ships
     end
 
     protected
@@ -35,10 +41,12 @@ module GE
       @players = get_int_input("How many players (1-20)") do |val|
         (1..20).include?(val)
       end
+      @player_worlds = (0...players)
 
       @worlds = get_int_input("How many worlds (#{players}-40)") do |val|
         (players..40).include?(val)
       end
+      @extra_worlds = (players...worlds)
 
       @turns = get_int_input("How many turns in the game (1-100).") do |val|
         (1..100).include?(val)
@@ -76,19 +84,32 @@ module GE
       cursor_position(1, 22)
     end
 
-    # These are all as-yet-unnamed procs that are the targets of GOSUBs
-
     # 2380
-    def e
+    def generate_industries
+      @industries = Array.new(players, 10) # player worlds
+      @industries += (extra_worlds).map{rand(1..5)} # neutral worlds
     end
 
     # 2460
-    def f
+    def set_initial_world_control
+      @controls = player_worlds.to_a
     end
 
     # 2510
-    def g
+    def build_ships
+      player_worlds.each do |pw|
+        denom = extra_worlds.map{|ew| 100 / (distance(pw, ew) + 2)}.sum
+        ships[pw] = worlds * (5 + 400/denom)
+      end
+      extra_worlds.each do |ew|
+        # TODO: something weird here ... on line 2640, it looks like a
+        # third of the time (randomly) it will repeat this step for
+        # the current star. Am I reading that correctly?
+        ships[ew] += rand(0...industries[ew]) + 3
+      end
     end
+
+    # These are all as-yet-unnamed procs that are the targets of GOSUBs
 
     # 2680
     def h
@@ -161,6 +182,11 @@ module GE
 
     def cursor_position(x, y)
       print "\e[#{y};#{x}H"
+    end
+
+    def distance(w1, w2)
+      radicand = (stars[w1][0] - stars[w2][0]) ** 2 + (stars[w1][1] - stars[w2][1]) ** 2
+      Math.sqrt(radicand).to_i
     end
   end
 end
